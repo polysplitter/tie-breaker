27e181df1b96246811191c6773d66a1ac312e8d9
Kyle <polysplitter@gmail.com>
2018-12-22 15:53:05 -0600

    fixed formatting.

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
-var rndKyle = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".img1").setAttribute("src", "images/dice" + rndKyle + ".png")
+var rndPlayer1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src", "images/dice" + rndPlayer1 + ".png")
 
-var rndJohn = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".img2").setAttribute("src", "images/dice" + rndJohn + ".png")
+var rndPlayer2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img2").setAttribute("src", "images/dice" + rndPlayer2 + ".png")
 
-if (rndKyle === rndJohn) {
+if (rndPlayer1 === rndPlayer2) {
   document.querySelector("h1").textContent = "Draw!"
-} else if (rndKyle > rndJohn) {
+} else if (rndPlayer1 > rndPlayer2) {
   document.querySelector("h1").textContent = "Player 1 Wins!"
-} else if ( rndKyle < rndJohn) {
+} else if (rndPlayer1 < rndPlayer2) {
   document.querySelector("h1").textContent = "Player 2 Wins!"
 }
